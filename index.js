89c8d23bb8798adbae2afc921b9889a38f3d2253
David <barodavid9@gmail.com>
2023-02-11 22:40:29 +0530

    First commit from local repository

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+
+// Initializing new instance of socket.io
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+const path = require('path');
+const port = 3000;
+const userNames = {};
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    // res.send('<h1>Chatter App</h1>');
+    res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+io.on('connection', (socket) => {
+    // console.log('a user connected');
+
+    // Getting name for newly joined user
+    socket.on('new-user-joined', (userName) => {
+        console.log(`${userName} joined the chat`);
+        userNames[socket.id] = userName;
+        socket.userName = userName;
+        socket.broadcast.emit('user-joined', userNames[socket.id]);
+    });
+    socket.on('disconnect', () => { // Getting name for disconnected user
+        console.log(`${socket.userName} left the chat`);
+        socket.broadcast.emit('user-left', userNames[socket.id]);
+        delete userNames[socket.id];
+    });
+    
+    // Listening for message sent by user
+    socket.on('chat message', (msg) => {
+        console.log('message from ' + socket.userName +": " + msg);
+        socket.emit('send', msg);
+        socket.broadcast.emit('receive', {msg: msg, userName: userNames[socket.id]});
+    });
+});
+
+server.listen(port, () => {
+    console.log(`Listening on *:${port}`);
+});
---
+++ public/js/client.js
@@ -0,0 +1,65 @@
+var socket = io();
+
+var form = document.getElementById('send-container');
+var input = document.getElementById('messageInp');
+
+const userName = prompt("Enter your name to join");
+socket.emit('new-user-joined', userName);
+
+// Append function messages sent/received
+// const appendMsg = (message, possition) => {
+//     const messageElement = document.createElement("div");
+//     messageElement.innerHTML = message;
+//     messageElement.classList.add('message'); 
+//     messageElement.classList.add(possition);
+
+// }
+
+
+// New user joined broadcast
+socket.on('user-joined', (userName) => {
+    // alert(`${userName} joined the chat`);
+    const para = document.createElement("div");
+    para.innerHTML = `${userName} joined the chat`;
+    para.classList.add('newuser-box');
+    // para.classList.add('userjoined-center');
+    document.getElementById('box').appendChild(para);
+});
+
+// User left broadcast
+socket.on('user-left', (userName) => {
+    // alert(`${userName} left the chat`);
+    const para = document.createElement("div");
+    para.innerHTML = `${userName} left the chat`;
+    para.classList.add('newuser-box');
+    // para.classList.add('userjoined-center');
+    document.getElementById('box').appendChild(para);
+});
+
+// Listening for message sent event/form submit
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    if(input.value) {
+        socket.emit('chat message', input.value);
+        input.value = '';
+    }
+});
+
+// Displaying user own message to them on the right
+socket.on('send', msg => {
+    const para = document.createElement("div");
+    para.innerHTML = `${msg}`;
+    para.classList.add('message');
+    para.classList.add('right');
+    document.getElementById('box').appendChild(para);
+})
+
+// Displaying other users messges on the left
+socket.on('receive', (data) => {
+    const para = document.createElement("div");
+    para.innerHTML = `<b> ${data.userName}:</b> <br> ${data.msg}`;
+    para.classList.add('message');
+    para.classList.add('left');
+    document.getElementById('box').appendChild(para);
+});
+
